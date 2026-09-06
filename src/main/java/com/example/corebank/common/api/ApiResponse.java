72bfd482a067b804912d6ef023f4f60f5362ae3e
Hoang Van Thanh <thoanghoangit57pm@gmail.com>
2026-09-06 15:32:41 +0700

    refactor: add field success, errorCode and delete field status

--- src/main/java/com/example/corebank/common/api/ApiResponse.java
+++ src/main/java/com/example/corebank/common/api/ApiResponse.java
@@ -6,22 +6,23 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record ApiResponse<T>(
-        ApiStatus status,
+        boolean success,
+        String errorCode,
         String message,
         T data,
         List<ErrorDetail> errors,
         LocalDateTime timestamp
 ){
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(ApiStatus.SUCCESS, "OK", data, null, LocalDateTime.now());
+        return new ApiResponse<>(true, null, "OK", data, null, LocalDateTime.now());
     }
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(ApiStatus.SUCCESS, message, data, null, LocalDateTime.now());
+        return new ApiResponse<>(true, null, message, data, null, LocalDateTime.now());
     }
-    public static <T> ApiResponse<T> error(ApiStatus status, String message) {
-        return new ApiResponse<>(status, message, null, null, LocalDateTime.now());
+    public static <T> ApiResponse<T> error(String errorCode, String message) {
+        return new ApiResponse<>(false, errorCode, message, null,null, LocalDateTime.now());
     }
     public static <T> ApiResponse<T> validationError(List<ErrorDetail> errors) {
-        return new ApiResponse<>(ApiStatus.VALIDATION_ERROR,"Dữ liệu không hợp lệ.", null, errors, LocalDateTime.now());
+        return new ApiResponse<>(false, "VALIDATION_ERROR", "Dữ liệu không hợp lệ.", null, errors, LocalDateTime.now());
     }
 }
