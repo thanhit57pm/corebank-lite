b9e171fef367066715411d861eb57d256805289e
Hoang Van Thanh <thoanghoangit57pm@gmail.com>
2026-09-07 15:16:37 +0700

    feat: add BaseEntity with JPA Auditing support

---
+++ src/main/java/com/example/corebank/core/base/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.example.corebank.core.base;
+
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updateAt;
+}
